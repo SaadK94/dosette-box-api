04b5ec7f530d0abae33f2917b20d1d9b8fe46c02
SaadK94 <Saad@saads-mbp.lan>
2020-06-24 14:42:10 +0100

    first deploy

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
 const app = express();
-// TODO: Add config library
 
 const mongoConnUri = process.env.MONGO_CONN_URI || 'mongodb://127.0.0.1:27017';
 const databaseName = process.env.DATABASE_NAME || 'dosette-app';
@@ -13,7 +12,6 @@ const uri = `${mongoConnUri}/${databaseName}`;
 mongoose.connect(uri, { useMongoClient: true });
 mongoose.connection.on('error', (error) => console.log(error));
 
-// TODO: Understand if this should sit here or elsewhere
 require('./lib/passport');
 
 app.use(cors());
@@ -24,14 +22,9 @@ const dosetteRoutes = require('./routes/dosette');
 const medicineRoutes = require('./routes/medicine');
 
 app.use('/', authRoutes);
-
-// TODO: Have doesette to be empty and ask the user to create a new once, which will become blank.
-// - Provide it a name
-//We plugin our jwt strategy as a middleware so only verified users can access this route
 app.use('/dosette', passport.authenticate('decode-jwt', { session: false }), dosetteRoutes);
 app.use('/medicine', passport.authenticate('decode-jwt', { session: false }), medicineRoutes);
 
-// Server set up
 app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.json({ error: err });
